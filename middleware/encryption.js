6e4933c97a9a9d68a2fd832a3712f739a02c1896
Zineb-S <75624026+Zineb-S@users.noreply.github.com>
2024-06-22 11:59:28 +0100

    Add detailed debug logs and test route for encryption consistency errorss

--- middleware/encryption.js
+++ middleware/encryption.js
@@ -1,16 +1,28 @@
 const crypto = require('crypto');
-const { publicKey, privateKey } = require('./keys');
+const { publicKey, privateKey } = require('../middleware/keyLoader');
 
+// Function to encrypt data
 const encrypt = (data) => {
-  const buffer = Buffer.from(data, 'utf8');
-  const encrypted = crypto.publicEncrypt(publicKey, buffer);
-  return encrypted.toString('base64');
+  try {
+    const buffer = Buffer.from(data, 'utf8');
+    const encrypted = crypto.publicEncrypt(publicKey, buffer);
+    return encrypted.toString('base64');
+  } catch (err) {
+    console.error('Error encrypting data:', err);
+    throw err;
+  }
 };
 
-const decrypt = (encrypted) => {
-  const buffer = Buffer.from(encrypted, 'base64');
-  const decrypted = crypto.privateDecrypt(privateKey, buffer);
-  return decrypted.toString('utf8');
+// Function to decrypt data
+const decrypt = (encryptedData) => {
+  try {
+    const buffer = Buffer.from(encryptedData, 'base64');
+    const decrypted = crypto.privateDecrypt(privateKey, buffer);
+    return decrypted.toString('utf8');
+  } catch (err) {
+    console.error('Error decrypting data:', err);
+    throw err;
+  }
 };
 
 module.exports = { encrypt, decrypt };
