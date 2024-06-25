6c66fb8246eb9d4629b0b27fb5da3506a256bdf0
Zineb-S <75624026+Zineb-S@users.noreply.github.com>
2024-06-25 22:34:29 +0100

    ngrok keycloak 2 client

--- config/keycloak.js
+++ config/keycloak.js
@@ -1,26 +1,25 @@
-(async () => {
-    const { default: KcAdminClient } = await import('@keycloak/keycloak-admin-client');
-  
-    const kcAdminClient = new KcAdminClient({
-      baseUrl: 'https://81f4-196-74-216-117.ngrok-free.app', // Keycloak server URL
-      realmName: 'Elected',             // Default realm name
+import KcAdminClient from '@keycloak/keycloak-admin-client';
+
+const kcAdminClient = new KcAdminClient({
+  baseUrl: 'https://81f4-196-74-216-117.ngrok-free.app', // Keycloak server URL
+  realmName: 'Elected',             // Default realm name
+});
+
+const initKeycloak = async () => {
+  try {
+    await kcAdminClient.auth({
+      grantType: 'client_credentials',
+      clientId: 'admin-cli',        // Client ID for admin
+      clientSecret: '39ykECnZMtFLgsDuaUifc5OZStKgRNco', // Replace with your actual client secret
     });
-  
-    const initKeycloak = async () => {
-      try {
-        await kcAdminClient.auth({
-          grantType: 'client_credentials',
-          clientId: 'admin-cli',        // Client ID for admin
-          clientSecret: '39ykECnZMtFLgsDuaUifc5OZStKgRNco', // Replace with your actual client secret
-        });
-        console.log("Keycloak Admin Client initialized successfully");
-      } catch (error) {
-        console.error("Error initializing Keycloak Admin Client", error);
-      }
-    };
-  
-    await initKeycloak();
-  
-    module.exports = kcAdminClient;
-  })();
-  
+    console.log("Keycloak Admin Client initialized successfully");
+    return kcAdminClient;
+  } catch (error) {
+    console.error("Error initializing Keycloak Admin Client", error);
+    throw error;
+  }
+};
+
+const kcAdminClientPromise = initKeycloak();
+
+export default kcAdminClientPromise;
